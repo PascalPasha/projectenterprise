924317cfb3804412eba65aca22518f258c1207f5
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2022-04-05 14:30:15 +0700

    first

---
+++ src/main/java/com/example/kelas/b/enterprise/DataController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.kelas.b.enterprise;
+
+/**
+ *
+ * @author pascal
+ */
+public class DataController {
+    
+}
---
+++ src/test/java/com/example/kelas/b/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kelas.b.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
